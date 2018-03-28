c580503cf3dcaea361b4d23fe5281a7e37c1f2f6
CJ May <cmax4jam@users.noreply.github.com>
2018-03-28 04:44:12 -0400

    almost finished mvp

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,16 @@
 function validate(){
-    var txt = $("#DOB").val();
-    var dob = new Date(txt);
-    if(dob >= new Date()){
-        alert("Date of Birth is not valid. Date has to be before today.");
-        return false;
-    }
 
-    var phoneNum = /^\d{10}$/;
-    if($("#phone").val().match(phoneNum))
+    var zip = /^\d{5}$/;
+    if($("#zip").val().match(zip))
     {
         return true;
     }
     else
     {
-        alert("Phone number is not valid or ont in a valid format.");
+        alert("The zip code is not valid.");
         return false;
     }
 }
-/*
-function dropdown() {
-    document.getElementById("occassions-dropdown").classList.toggle("show");
-}
 
-window.onclick = function(event) {
-    if(!event.target.matches('.dropbtn')){
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for(i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains('show')){
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-};
 
-*/
+
